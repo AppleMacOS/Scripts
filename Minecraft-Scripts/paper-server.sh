0ac69cc58903946960493d2d632a7bdcd78b0685
AppleMacOS <63877394+AppleMacOS@users.noreply.github.com>
2021-03-25 20:19:48 -0400

    Little clean up

--- Minecraft-Scripts/paper-server.sh
+++ Minecraft-Scripts/paper-server.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Color variables
-INFO='\033[1;32m'
-ERROR='\033[1;31m'
-RESET='\033[0m'
+INFO='\033[1;32m[INFO]\033[0m'
+ERROR='\033[1;31m[ERROR]\033[0m'
 
 # Checks if you have the required programs installed for the script to work
-command -v jq >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have jq installed on your system for this script to work. How to install: https://stedolan.github.io/jq/download/"; exit 1; }
-command -v wget >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have wget installed on your system for this script to work.  How to install: https://phoenixnap.com/kb/wget-command-with-examples"; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have jq installed on your system for this script to work. How to install: https://stedolan.github.io/jq/download/"; exit 1; }
+command -v wget >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have wget installed on your system for this script to work.  How to install: https://phoenixnap.com/kb/wget-command-with-examples"; exit 1; }
 
 # Asks for the name of the directory you want
 echo "Please enter the name of the server directory"; read server_directory
 
 # Checks to see if the provided directory exists
 if [ -d "$server_directory" ]; then
-    echo -e "$ERROR[ERROR]$RESET The provided directory already exists."
-    echo -e "$ERROR[ERROR]$RESET Please provide another directory name or delete the provided directory."
+    echo -e "$ERROR The provided directory already exists."
+    echo -e "$ERROR Please provide another directory name or delete the provided directory."
     exit 1
 fi
 
@@ -27,16 +26,16 @@ version_number=`curl -sS -X GET "https://papermc.io/api/v2/projects/paper/versio
 
 # Checks to see if the version that was provided doesn't exist
 if [ $version_number == "null" ]; then
-    echo -e "$ERROR[ERROR]$RESET The Paper version you provided is invalid."
+    echo -e "$ERROR The Paper version you provided is invalid."
     exit 1
 fi
 
 # Makes the server directory with the provided name 
-echo -e "$INFO[INFO]$RESET Making server directory."; mkdir $server_directory
+echo -e "$INFO Making server directory."; mkdir $server_directory
 # Downloads the requested versiuon of Paper
-echo -e "$INFO[INFO]$RESET Downloading Paper $version..."; wget https://papermc.io/api/v2/projects/paper/versions/$version/builds/$version_number/downloads/paper-$version-$version_number.jar &> /dev/null; echo -e "$INFO[INFO]$RESET Finished downloading Paper $version!"
+echo -e "$INFO Downloading Paper $version..."; wget https://papermc.io/api/v2/projects/paper/versions/$version/builds/$version_number/downloads/paper-$version-$version_number.jar &> /dev/null; echo -e "$INFO Finished downloading Paper $version!"
 # Moves the Paper jar to the server directory
-echo -e "$INFO[INFO]$RESET Moving jar file to server directory."; mv paper-$version-$version_number.jar ./$server_directory/
+echo -e "$INFO Moving jar file to server directory."; mv paper-$version-$version_number.jar ./$server_directory/
 
 # Yes or no function
 function yes_or_no() {
@@ -49,7 +48,7 @@ function yes_or_no() {
 
 # Asks the user if they agree to the Minecraft EULA
 if [[ "no" == $(yes_or_no "Do you agree to the Minecraft EULA?") ]]; then
-    echo -e "$INFO[INFO]$RESET You'll have to manually set the EULA to true"
+    echo -e "$INFO You'll have to manually set the EULA to true"
     exit 0
 fi
 
@@ -60,13 +59,13 @@ touch eula.txt; printf "eula=true" >> eula.txt
 
 # Asks if you want to start the server right now
 if [[ "no" == $(yes_or_no "Do you want to start the server now? Must have screen installed!") ]]; then
-    echo -e "$INFO[INFO]$RESET Script has finished!"
+    echo -e "$INFO Script has finished!"
     exit 0
 fi
 
 # Checks if screen and java is installed 
-command -v screen >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have screen installed on your system to start the server. How to install: https://phoenixnap.com/kb/how-to-use-linux-screen-with-commands"; exit 1; }
-command -v java >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have java installed on your system to start the server."; exit 1; }
+command -v screen >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have screen installed on your system to start the server. How to install: https://phoenixnap.com/kb/how-to-use-linux-screen-with-commands"; exit 1; }
+command -v java >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have java installed on your system to start the server."; exit 1; }
 
 # Starts the server
-echo -e "$INFO[INFO]$RESET Starting screen session!"; screen -S minecraft -dm java -Xms2G -Xmx2G -jar paper-$version-$version_number.jar; echo -e "$INFO[INFO]$RESET You can see the server console with screen -r minecraft"
+echo -e "$INFO Starting screen session!"; screen -S minecraft -dm java -Xms2G -Xmx2G -jar paper-$version-$version_number.jar; echo -e "$INFO You can see the server console with screen -r minecraft"
--- Minecraft-Scripts/vanilla-server.sh
+++ Minecraft-Scripts/vanilla-server.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Color variables
-INFO='\033[1;32m'
-ERROR='\033[1;31m'
-RESET='\033[0m'
+INFO='\033[1;32m[INFO]\033[0m'
+ERROR='\033[1;31m[ERROR]\033[0m'
 
 # Checks if you have the required programs installed for the script to work
-command -v wget >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have wget installed on your system for this script to work.  How to install: https://phoenixnap.com/kb/wget-command-with-examples"; exit 1; }
-command -v jq >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have jq installed on your system for this script to work. How to install: https://stedolan.github.io/jq/download/"; exit 1; }
+command -v wget >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have wget installed on your system for this script to work.  How to install: https://phoenixnap.com/kb/wget-command-with-examples"; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have jq installed on your system for this script to work. How to install: https://stedolan.github.io/jq/download/"; exit 1; }
 
 # Asks for the name of the directory you want
 echo -e "Please enter the name of the server directory"; read server_directory
 
 # Checks to see if the provided directory exists
 if [ -d "$server_directory" ]; then
-    echo -e "$ERROR[ERROR]$RESET The provided directory already exists."
-    echo -e "$ERROR[ERROR]$RESET Please provide another directory name or delete the provided directory."
+    echo -e "$ERROR The provided directory already exists."
+    echo -e "$ERROR Please provide another directory name or delete the provided directory."
     exit 1
 fi
 
@@ -25,17 +24,17 @@ echo -e "Please enter the version of Minecraft you want to use"; read version
 version_hash=`curl -sS -X GET "https://gist.githubusercontent.com/AppleMacOS/b16b018937e7982e0d7fc650d659fdaf/raw/b1bfac79f9e5db321d8fd4a450b4e49c73fa703b/vanilla-versions.json" -H  "accept: application/json" | jq --arg v "$version" '.[$v]'`
 # Checks to see if the provided version exists
 if [ $version_hash == null ]; then
-    echo -e "$ERROR[ERROR]$RESET The Minecraft version you provided is invalid."
+    echo -e "$ERROR The Minecraft version you provided is invalid."
     exit 1
 fi
 # Removes the double qoutes from hash
 version_hash_1=`echo "$version_hash" | tr -d '"'`
 # Downloads the jar file
-echo -e "$INFO[INFO]$RESET Downloading jar file..."; wget https://launcher.mojang.com/v1/objects/$version_hash_1/server.jar &> /dev/null; echo -e "$INFO[INFO]$RESET Finished downloading."
+echo -e "$INFO Downloading jar file..."; wget https://launcher.mojang.com/v1/objects/$version_hash_1/server.jar &> /dev/null; echo -e "$INFO Finished downloading."
 # Makes server directory
-echo -e "$INFO[INFO]$RESET Making server directory."; mkdir $server_directory
+echo -e "$INFO Making server directory."; mkdir $server_directory
 # Moves server jar to server directory
-echo -e "$INFO[INFO]$RESET Moving the Server jar to the server directory."; mv server.jar ./$server_directory/
+echo -e "$INFO Moving the Server jar to the server directory."; mv server.jar ./$server_directory/
 
 # Yes or no function
 function yes_or_no() {
@@ -48,7 +47,7 @@ function yes_or_no() {
 
 # Asks the user if they agree to the Minecraft EULA
 if [[ "no" == $(yes_or_no "Do you agree to the Minecraft EULA?") ]]; then
-    echo -e "$INFO[INFO]$RESET You'll have to manually set the EULA to true"
+    echo -e "$INFO You'll have to manually set the EULA to true"
     exit 0
 fi
 
@@ -59,13 +58,13 @@ touch eula.txt; printf "eula=true" >> eula.txt
 
 # Asks if you want to start the server right now
 if [[ "no" == $(yes_or_no "Do you want to start the server now? Must have screen installed!") ]]; then
-    echo -e "$INFO[INFO]$RESET Script has finished!"
+    echo -e "$INFO Script has finished!"
     exit 0
 fi
 
 # Checks if screen and java is installed 
-command -v java >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have java installed on your system for this script to work."; exit 1; }
-command -v screen >/dev/null 2>&1 || { echo -e >&2 "$ERROR[ERROR]$RESET You most have screen installed on your system to start the server. How to install: https://phoenixnap.com/kb/how-to-use-linux-screen-with-commands"; exit 1; }
+command -v java >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have java installed on your system for this script to work."; exit 1; }
+command -v screen >/dev/null 2>&1 || { echo -e >&2 "$ERROR You most have screen installed on your system to start the server. How to install: https://phoenixnap.com/kb/how-to-use-linux-screen-with-commands"; exit 1; }
 
 # Starts the server
-echo -e "$INFO[INFO]$RESET Starting screen session!"; screen -S minecraft -dm java -Xms2G -Xmx2G -jar server.jar --nogui; echo -e "$INFO[INFO]$RESET You can see the server console with screen -r minecraft"
+echo -e "$INFO Starting screen session!"; screen -S minecraft -dm java -Xms2G -Xmx2G -jar server.jar --nogui; echo -e "$INFO You can see the server console with screen -r minecraft"
